b90bce6066c89a8117ce4990da47a4c6187645ab
Betasolutions1 <beta.solutions@mail.com>
2017-04-03 18:09:19 +0530

    devedlop work

---
+++ forum_try.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title>LN BUSINESS</title>
+        <!-- Bootstrap core CSS -->
+        <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/font/font-awesome/css/font-awesome.min.css">
+	
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script src="assets/js/jquery.mousewheel.js"></script>
+	<script>
+	$(function(){
+		$("#page-wrap").wrapInner("<table cellspacing=35><tr>");
+		$(".post").wrap("<td></td>");
+		$("body").mousewheel(function(event, delta) {
+			this.scrollLeft -= (delta * 30);
+			event.preventDefault();
+		});   
+	});
+	</script>
+    <style>
+	hr{
+		color:#fff;
+	}
+	</style>
+</head>
+<body>
+	<div>
+<nav class="navbar navbar-inverse navbar-fixed-top" style="height:10%;">
+            <div class="container">
+                <div class="navbar-header">
+                    <!-- <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button> -->
+                    <a class="navbar-brand" href="#" style="font-family: Stencil Std;color: #808080;">
+    LN BUSINESS</a>
+                </div>
+                <div id="navbar" class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+
+                        <li style=" padding-left:30px; padding-top:10px; padding-right:100px; ">
+                        	<div style="background-color:#808080;padding-right:5px;">
+                            <input type="text" style="width:350px; height:30px;background-color:#808080; border:0px; color:#fff;margin-left:5px;">
+                            <i class="fa  fa-2x fa-search" style="color:#fff; padding-top:5px;"></i>
+                        </div>
+                        </li>
+                        <li class="active">
+                            <a href="#" style="font-family:Stencil Std; font-size:24px; padding-left:25px; padding-right:25px; color: #fff;">F</a>
+                        </li>
+                        <li>
+                            <a href="#about" style="font-family:Stencil Std; font-size:24px; padding-left:25px; padding-right:25px; color: #808080;">M</a>
+                        </li>
+                                               <li style="height:50px;line-height:13px;padding-top:3px;">
+                            <p class="diary" style="padding-top:0px; padding-left:50px;color:#808080"><span class="day" style="font-family:Stencil Std; font-size:10px;">friday</span><br><span class="month" style="font-family:Stencil Std; font-size:10px;">March</span><br><span class="year" style="font-family:Stencil Std; font-size:10px;">2017</span></p>
+                        </li>
+                        <li style="height:50px;">
+                            <!--<hr style="width:0px; height:10px;" class="vertical" />-->
+                            <div style="border-left:2px solid #d3d3d3;margin-top:6px; margin-left:4px; margin-right:4px;height:40px;" class="line_div">
+</div>
+                        </li>
+                        <li style="height:50px; float:right;">
+                            <span class="date" style="font-family:Stencil Std; font-size:40px; padding-top:5px; padding-right:10px;color:#808080;">31</span>
+                        </li>
+                    </ul>
+                </div>
+                <!--/.nav-collapse style="height:600px;background-color:#fff; float:left" -->
+            </div>
+        </nav>
+        <div class="paper">
+         <div class="container-fluid" style="padding-top:10px;">
+         
+         
+            <div class="row">
+            
+            <!--recent topics -->
+                <div class="col-lg-3" style="height:500px; float:left;border:1px solid #D9D7D7;margin-top:75px">
+                <div class="row">
+                <div class="col-lg-12">
+                <div ><h3 align="right" style="border-bottom:4px solid #Dbd8d8;"><span ><b style="border-bottom:12px solid #Dbd8d8;margin-right:8px;">Recent Topics</b></span></h3></div>
+                <ul>
+                <li style="border-bottom:1px solid  #DBD8D8;list-style:none;padding:6px;">ft rx y</li>
+                <li style="border-bottom:1px solid  #DBD8D8;list-style:none;padding:6px;">ft rx y</li>
+                <li style="border-bottom:1px solid  #DBD8D8;list-style:none;padding:6px;">ft rx y</li>
+                <li style="border-bottom:1px solid  #DBD8D8;list-style:none;padding:6px;">ft rx y</li>
+                </ul>
+                </div>
+                  
+                   
+                   
+					</div>
+                </div>
+                <!--end recent topics-->
+                
+               <!--forums begin-->
+               
+                
+                <div class="col-lg-9" style="height:500px; float:right;border:1px solid #D9D7D7;margin-top:75px">
+              
+               
+                
+              
+                
+                
+                
+                </div>
+            </div>
+         </div>
+        </div>
+        </div>
+        <!--footer-->
+        <div>
+        
+    <footer >
+    </footer>
+</div>
+<br>
+
+    </body>
+    </html>
